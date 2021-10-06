f4a2ce372e3888159edcae0c9ada4fbfd6478ca5
Javier Fernandez fernandez <javferna@c2r12s6.42madrid.com>
2021-10-06 16:19:06 +0200

    changed substr_gnl (timeout valgrind) no bonus

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: javferna <javferna@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 16:18:56 by javferna          #+#    #+#             */
-/*   Updated: 2021/10/06 15:53:29 by javferna         ###   ########.fr       */
+/*   Updated: 2021/10/06 16:14:07 by javferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
 char	*ft_strchr(const char *s, int c);
 
 #endif
